6f23d8c6ad85521c5ec7bccbe8e66824a01b0f2a
Brandon <brandonesser435@gmail.com>
2021-12-27 16:39:05 -0600

    the debunging update

--- Cipher.py
+++ Cipher.py
@@ -1,91 +1,129 @@
+def notes():
+    """
+    "Encryption is poggers" -Alan Turing, probably 
 
-version = "v1.0"
-import string
+    Very bad explanaion because the key and encoded message are the same
+    Generate and assign a random character to a letter in a message
+    The key is the alphabet in order, but with random letters
+    Decoder takes key, see what letter/number/symbol is assigned to the alphabet’s letter, and then decodes the message
+    If I'm not mistaken, its a type of a transposition cipher.
+
+
+    Message: abcd
+    key: a = g, b = 1, c = z, d = & 
+    Encoded message: g1z&
+
+    Key: g1z&
+    G is in the A spot, 1 is in the B spot, z is in the C spot, & is in the D spot
+    So decoded it is: abcd
+
+
+    This could probably be cracked in the span of about 0.00000000001 seconds.
+
+    """
+version = "v0.2 (unbunged version of 0.1)"
+debug = 0
+
+
 import random
-"""
-NOTE
-"Encryption is poggers" -Alan Turing, probably
+import string
+import os
+
+def main():
+    def cc():   # shortens long command to just cc()
+        os.system("cls" if os.name == "nt" else "clear")
 
 
-This is a very simple method I came up with, but I'm sure its already been discovered and has a proper name
+    def plain_text_setup():
+        global plain_in_char_count
+        global plain_in
+
+        plain_in = "Hello World"
+        plain_in_char_count = len(plain_in)
+    
+
+    def key_gen():
+        global shuffled_chars
+        global random_chars
+        global key_out
+
+        # all the characters that can make up the key
+        all_chars = list(string.ascii_letters + string.digits + 
+        string.punctuation)  # string.printable create problems. thats why this war crime soup is being used
+
+        # same thing as above but randomized
+        random.shuffle(all_chars)
+        shuffled_chars = "".join(all_chars)
 
 
-Very bad explanaion considering the message is part of the key lol
-Generate and assign a random character to a letter in a message
-Key is the alphabet in order, but with random letters
-Decoder takes key, see what letter/number/symbol is assigned to the alphabet’s letter, and then decodes the message
+        key_out = all_chars[0:26] 
+        """
+        Ideas:
+        Try getting rid of limit, maybe the encoder could prefix the key with a number.
+        That number would indicate how far in the key is
 
-Message: abcd
-key: a = g, b = 1, c = z, d = & 
-Encoded message: g1z&
+        Maybe generate random length caracter chunks, and randomly assign a chunk to a letter
 
-Key: g1z&
-G is in the a spot, 1 is in the b spot, z is in the c spot, & is in the d spot
-So decoded it is: abcd
-
-
-This could probably be cracked in the span of about 10 seconds.
-Once you know how it works, all the data is there to decode it.
-"""
-
-
-#string.printable 
-#chars = string.printable    # possible key characters
-chars = string.ascii_letters
-rand_char_count = len(chars)
-
-
-def decode():
-    to_decode = input("What is the message you would like to decode? ")
-    decode_key = input("Enter the key: ")
-
-
-
-
-
-
-
-
-    #print("Decoded message: " + decoded_msg)
-
-
-
-def encode():
-    random_key = []     
-    encoded_msg = ""    # the final message
-    msg_in = input("What is the message you would like to encode? ")    # what to encode
-
-
-    for i in range(rand_char_count):     
-        char_choice = random.sample(chars, 1)
-        random_key.append(char_choice)
-
-
-    list_t_str = "".join([str(elem) for elem in random_key])
-    #list_t_str = str(random_key)    # list to string for the thing below
+        Combine the two
+        This is awful
+        Do the above ideas actually improve security? Probably not.
+        """
 
     
-    for letter in msg_in:
-        if letter.lower() in chars:
-            encoded_msg += list_t_str[chars.find(letter.lower())]   
+    def encode_msg(key):
+        
+        cyphertext = "" # final message
+
+        print(key)
+        
+        
+        
+        
+        print("Your key is: ", end="") 
+        print(*key_out, sep="")
+        print("Your encoded message is: ")
+        print()
+        input("Press enter to continue")
+        cc()
+        menu()
+
+
+    def decode_msg():
+        pass
+
+
+    def print_debug():    # stuff to print if debug moce is enabled
+        global debug
+        if debug:
+            print()
+            print("DEBUG:")
+            print(version)
+            print("Plain text input: " + plain_in)
+            print("Input char count: " + str(plain_in_char_count))
+            print("All chars shuffled: " + shuffled_chars)
+            print()
+
+
+    def menu():
+        
+
+        print("Besser's redneck encryption. 100% secure, I would never lie.")
+        print_debug()
+        print(version)
+
+        ask_type = input("Would you like to encode or decode a message? e/d ")
+        if ask_type == "e":
+            cc()
+            plain_text_setup()
+            key_gen()
+            encode_msg(key_out)
+
+        elif ask_type == "d":
+            pass
+
         else:
-            encoded_msg += letter
+            pass
 
 
-    print("Encoded message: " + encoded_msg)
-    print()
-    #print("Your key is: ", *encode_key, sep = "")
-
-
-
-
-action = input("Would you like to encode or decode? e/d ")
-
-if action == "e":
-    encode()
-elif action == "d":
-    decode()
-else:
-    print("Invalid input")
-
-
+    menu()
+main()
---
+++ Cipher_old.py
@@ -0,0 +1,90 @@
+
+version = "v0.1"
+import string
+import random
+"""
+NOTE
+"Encryption is poggers" -Alan Turing, probably
+
+
+This is a very simple method I came up with, but I'm sure its already been discovered and has a proper name
+
+
+Very bad explanaion considering the message is part of the key lol
+Generate and assign a random character to a letter in a message
+Key is the alphabet in order, but with random letters
+Decoder takes key, see what letter/number/symbol is assigned to the alphabet’s letter, and then decodes the message
+
+Message: abcd
+key: a = g, b = 1, c = z, d = & 
+Encoded message: g1z&
+
+Key: g1z&
+G is in the a spot, 1 is in the b spot, z is in the c spot, & is in the d spot
+So decoded it is: abcd
+
+
+This could probably be cracked in the span of about 10 seconds.
+Once you know how it works, all the data is there to decode it.
+"""
+
+
+#string.printable 
+#chars = string.printable    # possible key characters
+chars = string.ascii_letters
+rand_char_count = len(chars)
+string.digits
+
+
+
+
+def decode():
+    to_decode = input("What is the message you would like to decode? ")
+    decode_key = input("Enter the key: ")
+
+
+
+    #print("Decoded message: " + decoded_msg)
+
+def key_gen():
+    pass
+
+def encode():
+    random_key = []     
+    encoded_msg = ""    # the final message
+    msg_in = input("What is the message you would like to encode? ") 
+
+
+    for i in range(rand_char_count):     
+        char_choice = random.sample(chars, 1)
+        random_key.append(char_choice)
+
+
+    list_t_str = "".join([str(elem) for elem in random_key])
+    #list_t_str = str(random_key)    # list to string for the thing below
+
+    
+    for letter in msg_in:
+        if letter.lower() in chars:
+            encoded_msg += list_t_str[chars.find(letter.lower())]   
+        else:
+            encoded_msg += letter
+
+
+    print("Encoded message: " + encoded_msg)
+    print()
+    #print("Your key is: ", *encode_key, sep = "")
+
+
+
+
+action = input("Would you like to encode or decode? e/d ")
+
+if action == "e":
+    encode()
+elif action == "d":
+    decode()
+else:
+    print("Invalid input")
+
+
